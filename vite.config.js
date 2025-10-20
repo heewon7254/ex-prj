92b79cf47525a219cd4221fd0e3b13a18b237950
heewon7254 <heewon7254@naver.com>
2025-10-20 21:46:28 +0900

    package json  페이지 설정

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/ex-prj/", // ✅ GitHub Pages용 base 경로 추가
   build: {
-    target: 'es2015', // es5+
+    target: "es2015",
   },
-  // 정적 파일 서빙이라서 브라우저 새로고침 시 라우팅 꺠짐 방지
   server: {
     historyApiFallback: true,
   },
